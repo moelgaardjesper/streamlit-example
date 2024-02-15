2bbc1675a1fe87047f44661ffd94fdbbc0a57afd
Jesper Mølgaard <46134369+moelgaardjesper@users.noreply.github.com>
2024-02-15 10:59:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,6 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 year = st.slider("Year", 1980, 2010, 2007)
 year_str = str(year)
 
